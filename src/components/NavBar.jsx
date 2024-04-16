606e25246f3a1b5e1fe0e721b84e726a9693828c
stephumpierrez <stephumpierrez@gmail.com>
2024-04-15 22:41:35 -0300

    Clase 3

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+    return(
+        <nav>
+            <h2>Componente NavBar</h2>
+            <ul>
+                <li>Teclados</li>
+                <li>Mouse</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
